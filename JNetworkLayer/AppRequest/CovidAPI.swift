2f167c13e816e68bd3d80f524a20c1bef6722937
Josue H <jghg.02@gmail.com>
2021-07-20 20:36:39 -0400

    Changed Networking methos to a swift package manager

--- JNetworkLayer/AppRequest/CovidAPI.swift
+++ JNetworkLayer/AppRequest/CovidAPI.swift
@@ -6,6 +6,7 @@
 //
 
 import Foundation
+import JNetworking
 
 struct CovidAPIQueryParams {
     let yesterday: Bool = true
--- JNetworkLayer/AppRequest/CovidServicesRequest.swift
+++ JNetworkLayer/AppRequest/CovidServicesRequest.swift
@@ -6,6 +6,7 @@
 //
 
 import Foundation
+import JNetworking
 
 typealias GetAllCountriesResponse = (Result<[Country], Error>) -> Void
 
